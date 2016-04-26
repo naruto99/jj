56e23aec5d3493724096fe46e1bd8bc33a4a7ea7
Rexy Leanax <miraculousprox@gmail.com>
2016-04-26 18:32:10 +0300

    updated hp bar

--- Berb.Common/LeagueSharp-Common/Utility.cs
+++ Berb.Common/LeagueSharp-Common/Utility.cs
@@ -34,6 +34,7 @@ using Color = System.Drawing.Color;
 
 namespace LeagueSharp.Common
 {
+
     /// <summary>
     ///     Game functions related utilities.
     /// </summary>
@@ -843,6 +844,63 @@ namespace LeagueSharp.Common
                 {
                     Time = time + Utils.GameTimeTickCount;
                     CallbackObject = callback;
+                }
+            }
+        }
+
+        public static class HpBarDamageIndicator
+        {
+            public delegate float DamageToUnitDelegate(AIHeroClient hero);
+
+            private const int XOffset = 10;
+            private const int YOffset = 20;
+            private const int Width = 103;
+            private const int Height = 8;
+            public static Color Color = Color.Lime;
+            public static bool Enabled = true;
+            private static DamageToUnitDelegate _damageToUnit;
+
+            private static readonly Render.Text Text = new Render.Text(
+                0, 0, string.Empty, 11, new ColorBGRA(255, 0, 0, 255), "monospace");
+
+            public static DamageToUnitDelegate DamageToUnit
+            {
+                get { return _damageToUnit; }
+
+                set
+                {
+                    if (_damageToUnit == null)
+                    {
+                        Drawing.OnDraw += Drawing_OnDraw;
+                    }
+                    _damageToUnit = value;
+                }
+            }
+
+            private static void Drawing_OnDraw(EventArgs args)
+            {
+                if (!Enabled || _damageToUnit == null)
+                {
+                    return;
+                }
+
+                foreach (var unit in
+                    HeroManager.Enemies.FindAll(h => h.IsValid && h.IsHPBarRendered))
+                {
+                    var barPos = unit.HPBarPosition;
+                    var damage = _damageToUnit(unit);
+                    var percentHealthAfterDamage = Math.Max(0, unit.Health - damage) / unit.MaxHealth;
+                    var xPos = barPos.X + XOffset + Width * percentHealthAfterDamage;
+
+                    //if (damage > unit.Health)
+                    {
+                        Text.X = (int)barPos.X + XOffset;
+                        Text.Y = (int)barPos.Y + YOffset - 13;
+                        Text.text = ((int)(unit.Health - damage)).ToString();
+                        Text.OnEndScene();
+                    }
+
+                    Drawing.DrawLine(xPos, barPos.Y + YOffset, xPos, barPos.Y + YOffset + Height, 2, Color);
                 }
             }
         }
