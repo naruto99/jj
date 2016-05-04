d2b33b07051186d8440171733dc1de74d8f79958
Rexy Leanax <miraculousprox@gmail.com>
2016-05-04 22:08:42 +0300

    ported

---
+++ TheCheater/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EloBuddy.SDK.Events;
+
+namespace WhoIsYourCheater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            TheCheater.TheCheater.Load();
+        }
+    }
+}
---
+++ TheCheater/TheCheater/IDetector.cs
@@ -0,0 +1,15 @@
+﻿using EloBuddy;
+using SharpDX;
+
+namespace WhoIsYourCheater.TheCheater
+{
+    public enum DetectorSetting { Preferred, Safe, AntiHumanizer}
+    interface IDetector
+    {
+        void Initialize(AIHeroClient hero, DetectorSetting setting = DetectorSetting.Safe);
+        void ApplySetting(DetectorSetting setting);
+        void FeedData(Vector3 targetPos);
+        int GetScriptDetections();
+        string GetName();
+    }
+}
